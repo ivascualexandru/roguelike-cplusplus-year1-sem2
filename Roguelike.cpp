54bee274acb0ccc1f7bbeb958be66e9e1de6566c
Alexandru Ivascu (ivascua) <ivascua@coventry.ac.uk>
2020-02-11 17:08:07 +0000

    Combat fully done
    I STILL HAVE TO FIND A WAY TO KEEP TEXT ON THE SCREEN AFTER A SCREEN WIPE FFS

--- Roguelike.cpp
+++ Roguelike.cpp
@@ -24,17 +24,45 @@ int main()
 		
 		system("pause > nul");		// usually the console closes after one key press but this allows the user to input more key presses
 
-		if (GetAsyncKeyState(0x57))			// 0x77 is the virtual key code for the w key, checks if w has been pressed
-			character.Movement(-1, 0);		// moves up
 
-		if (GetAsyncKeyState(0x53))			// 0x53 is the virtual key code for the w key, checks if w has been pressed
-			character.Movement(1, 0);		// moves down
 
-		if (GetAsyncKeyState(0x44))			// 0x44 is the virtual key code for the w key, checks if w has been pressed
-			character.Movement(0, 1);		// moves right
 
-		if (GetAsyncKeyState(0x41))         // 0x41 is the virtual key code for the w key, checks if w has been pressed
-		{	if ((character.x==goblin.x+1) && (character.y==goblin.y))
+
+
+		if (GetAsyncKeyState(0x57))										// moves up
+			{	
+				if ((character.x==goblin.x) && (character.y==goblin.y+1))
+			{
+				goblin.health-=character.attack;
+				if (goblin.health < 0){
+					cout<<"You killed him lmao";
+					map[goblin.x][goblin.y]=' ';
+					character.xp+=10;
+				}
+			}
+				if (((character.x==goblin.x-1) && (character.y==goblin.y)) || ((character.x==goblin.x+1) && (character.y==goblin.y)) || ((character.x==goblin.x) && (character.y==goblin.y+1)) || ((character.x==goblin.x) && (character.y==goblin.y-1)))
+				{
+					character.health-=goblin.attack;
+					if (character.health<0){
+						cout<<"You got killed by him lmao"<<endl;
+						cout<<"You got killed by him lmao"<<endl;
+						cout<<"You got killed by him lmao"<<endl;
+						cout<<"You got killed by him lmao"<<endl;
+						map[character.x][character.y]=' ';          //PLAYER DEATH
+				}
+		}
+			character.Movement(-1, 0);
+			}
+
+
+
+
+
+
+
+		if (GetAsyncKeyState(0x53))										// moves down
+			{	
+				if ((character.x==goblin.x) && (character.y==goblin.y-1))
 			{
 				goblin.health-=character.attack;
 				if (goblin.health < 0){
@@ -45,23 +73,99 @@ int main()
 			}
 		if (((character.x==goblin.x-1) && (character.y==goblin.y)) || ((character.x==goblin.x+1) && (character.y==goblin.y)) || ((character.x==goblin.x) && (character.y==goblin.y+1)) || ((character.x==goblin.x) && (character.y==goblin.y-1))){
 			character.health-=goblin.attack;
+			if (character.health<0){
+						cout<<"You got killed by him lmao"<<endl;
+						cout<<"You got killed by him lmao"<<endl;
+						cout<<"You got killed by him lmao"<<endl;
+						cout<<"You got killed by him lmao"<<endl;
+						map[character.x][character.y]=' ';          //PLAYER DEATH
+				}
 		}
-						
-			character.Movement(0, -1);      // moves left
+			character.Movement(1, 0);
+			}
+
+
+
+
+
+
+		if (GetAsyncKeyState(0x44))
+			{	
+				if ((character.x==goblin.x-1) && (character.y==goblin.y))
+			{
+				goblin.health-=character.attack;
+				if (goblin.health < 0){
+					cout<<"You killed him lmao";            			//moves right
+					map[goblin.x][goblin.y]=' ';
+					character.xp+=10;
+				}
+			}
+		if (((character.x==goblin.x-1) && (character.y==goblin.y)) || ((character.x==goblin.x+1) && (character.y==goblin.y)) || ((character.x==goblin.x) && (character.y==goblin.y+1)) || ((character.x==goblin.x) && (character.y==goblin.y-1))){
+			character.health-=goblin.attack;
+			if (character.health<0){
+						cout<<"You got killed by him lmao"<<endl;
+						cout<<"You got killed by him lmao"<<endl;
+						cout<<"You got killed by him lmao"<<endl;
+						cout<<"You got killed by him lmao"<<endl;
+						map[character.x][character.y]=' ';          //PLAYER DEATH
+				}
 		}
-		if (GetAsyncKeyState(0x50))         //0x50 is the virtual key code for the p key, checks if P has been pressed
-			{								//Hopefully creates a .txt file
+			character.Movement(0, 1);
+			}
+
+
+
+
+		if (GetAsyncKeyState(0x41))
+		{	if ((character.x==goblin.x+1) && (character.y==goblin.y))
+			{
+				goblin.health-=character.attack;
+				if (goblin.health < 0){
+					cout<<"You killed him lmao";				         // moves left
+					map[goblin.x][goblin.y]=' ';
+					character.xp+=10;
+				}
+			}
+		if (((character.x==goblin.x-1) && (character.y==goblin.y)) || ((character.x==goblin.x+1) && (character.y==goblin.y)) || ((character.x==goblin.x) && (character.y==goblin.y+1)) || ((character.x==goblin.x) && (character.y==goblin.y-1))){
+			character.health-=goblin.attack;
+			if (character.health<0){
+						cout<<"You got killed by him lmao"<<endl;
+						cout<<"You got killed by him lmao"<<endl;
+						cout<<"You got killed by him lmao"<<endl;
+						cout<<"You got killed by him lmao"<<endl;
+						map[character.x][character.y]=' ';          //PLAYER DEATH
+				}
+		}
+			character.Movement(0, -1);
+		}
+
+
+
+
+		
+		if (GetAsyncKeyState(0x50))
+			{
 				int playerx=character.x;
 				int playery=character.y;
 				int playerhealth=character.health;
-				int playerattack=character.attack;
+				int playerattack=character.attack;             //0x50 is the virtual key code for the p key, checks if P has been pressed
 				int playerxp=character.xp;
 				saveGame(playerx,playery,playerhealth,playerattack,playerxp);
 			}
+
+
+
+
 
 		if (GetAsyncKeyState(0x4C))
 			{
 				//loadGame();
 			}
+
+
+
+		if (GetAsyncKeyState(0x49)){
+			for (int i=0; i<8; i++) cout<<character.inventory[i];  //To be continued when Dimi's database becomes available
+		}
 	}
 }
