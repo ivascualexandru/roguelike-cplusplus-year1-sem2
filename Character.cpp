2788eeb9c63519f35f699a135d68376fe214c28b
Alexandru Ivascu (ivascua) <ivascua@coventry.ac.uk>
2020-03-03 10:09:24 +0000

    Update Character.cpp

--- Character.cpp
+++ Character.cpp
@@ -11,33 +11,10 @@ char map[15][15] = {			//static map in array
 	"*            *",
 	"*            *",
 	"*            *",
-	"*            *",
-	"*            *",
-	"*            *",
-	"*            *",
 	"**************"
 };
 
-void Character::Movement(int Vertical, int Horizontal)
-{
-	int x2 = x + Horizontal;	// movement on x-axis
-	int y2 = y + Vertical;		// movement on y-axis
 
-	if (map[y][x2] == ' ') // if player moves horizontally the space before becomes an empty space and the space moved to becomes '@'
-	{
-		map[y][x] = ' ';
-		x += Horizontal;
-		map[y][x] = '@';
-	}
-
-
-
-	if (map[y2][x] == ' ') // if player moves vertically the space before becomes an empty space and the space moved to becomes '@'
-	{
-		map[y][x] = ' ';
-		y += Vertical;
-		map[y][x] = '@';
-	}
 
 	/*if (map[y][x2] == '-') //attempt at corridors lmao
 	{
@@ -52,4 +29,3 @@ void Character::Movement(int Vertical, int Horizontal)
 		map[y][x] = '@';
 	}*/
 
-}
